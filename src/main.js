db46956eb69930166ce4296355bc0484581c6973
Jackie Sanchez <jsanc189@ucsc.edu>
2023-10-11 21:50:56 -0700

    updated the main, menu, play, and rocket files as well as added png assets for game sprites

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 let config = {
     type: Phaser.CANVAS,
     width: 640,
-    height: 480
+    height: 480,
+    scene: [Menu, Play]
 }
 
+//reserve keyboard vars
+let keyF, keyR, keyLEFT, keyRIGHT;
+
 let game = new Phaser.Game(config);
+
+
+//set User Interface sizes
+let borderUISize = game.config.height / 15;
+
+let borderPAdding = borderUISize / 3;
