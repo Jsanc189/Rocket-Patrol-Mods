472c86807bc7344f5281651dca32878e6e1eea4e
Jackie Sanchez <jsanc189@ucsc.edu>
2023-10-15 20:33:41 -0700

    added in difficulty levels and end of game return main menu option

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -69,15 +69,15 @@ class Play extends Phaser.Scene{
         scoreConfig.fixedWidth = 0;
         this.clock = this.time.delayedCall(this.timer, () => {
             this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', scoreConfig).setOrigin(0.5);
-            this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart', scoreConfig).setOrigin(0.5);
+            this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart or <= for Menu', scoreConfig).setOrigin(0.5);
             this.gameOver = true;
         }, null, this);
 
     }
 
     update() {
-        if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyR)) {
-            this.scene.restart();
+        if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyLEFT)) {
+            this.scene.start("menuScene");
         }
 
         this.starfield.tilePositionX -= 4;
@@ -135,7 +135,7 @@ class Play extends Phaser.Scene{
 
         this.p1Score += ship.points;
         this.scoreLeft.text = this.p1Score;
-        this.sound.play('sfc_explosion');
+        this.sound.play('sfx_explosion');
     }
 
 }
