53cb818c89b98ecb6625eff0779f9abbd420bf9f
Jackie Sanchez <jsanc189@ucsc.edu>
2023-10-15 19:35:35 -0700

    added in audio files to assets folder

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -3,6 +3,13 @@ class Menu extends Phaser.Scene{
         super("menuScene");
     }
 
+    preload() {
+        //load audio
+        this.preload.audio('sfx_select', './assets/blip_select.wav');
+        this.preload.audio('sfx_explosion', './assets/explosion.wav');
+        this.preload.audio('sfx_rocket', './assets/rocket.wav');
+    }
+
     create() {
         this.add.text(20, 20, "Rocket Patrol Menu");
         this.scene.start("playScene");
