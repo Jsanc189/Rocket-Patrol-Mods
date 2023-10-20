6c32c341b90a2ea8ce895d2d153baa001435d6f2
Jackie Sanchez <jsanc189@ucsc.edu>
2023-10-19 22:32:39 -0700

    added timer testing (timer not implemented yet)

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -158,6 +158,8 @@ class Play extends Phaser.Scene{
         });
 
         this.p1Score += ship.points;
+        this.timer += ship.points * 1000;
+
         this.scoreLeft.text = this.p1Score;
         this.sound.play(sound_effects[random_num]);
     }
